c4a06a5ccc12626fbef9b25b0496ae45620e01bb
Maryam-Ashfaq10 <maryammashfaq000@gmail.com>
2023-12-03 01:19:26 +0500

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# About
+This is a landing page created using React with Tailwindd css for styling.
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center max-w-[1240px] mx-auto text-white'>
+      <h1 className='w-full text-3xl font-bold '>WELCOME</h1>
+      <ul className='flex items-center'>
+        <li className='p-4'>CONTACT</li>
+       
+
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
