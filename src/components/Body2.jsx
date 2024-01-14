5f865e48449d89fc2f001a1ab5bad68842d820c0
Maryam-Ashfaq10 <maryammashfaq000@gmail.com>
2024-01-15 00:52:23 +0500

    updated

--- src/components/Body2.jsx
+++ src/components/Body2.jsx
@@ -11,7 +11,7 @@ const Body2 = () => {
             </div>
             <br/>
             <div>
-                <h1 className='text-[#1c2956] font-bold md:text-4xl sm:text-3xl text-2xl '>WEB AND APP</h1>
+                <h1 className='text-[#1c2956] font-bold md:text-4xl sm:text-3xl text-2xl '>WEB AND MOBILE</h1>
                 <p  className='py-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam soluta similique quaerat aliquam quia 
                     perferendis tempora exercitationem explicabo voluptatem obcaecati!</p>
                 <button className='bg-black text-[#a0d1f2] w-[200px] rounded-md my-5 mx-auto py-1 '>Get Started</button>    
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,11 +5,23 @@ const Cards = () => {
     <div className='w-full py-[9rem] px-4 text-white'>
       <div className='max-w-[1000px] mx-auto grid md:grid-cols-3 gap-6'>
         <div className='w-full border rounded-md hover:scale-105 duration-300'>
-        <h2 className='text-2xl font-bold text-center py-6'>100+</h2>
+        <h2 className='text-2xl font-bold text-center py-6'>200+</h2>
         <p className='text-center'>Projects completed</p>
         <p className='text-center'>Satisfied clients</p>
-
         </div>
+
+        <div className='w-full border rounded-md hover:scale-105 duration-300'>
+        <h2 className='text-2xl font-bold text-center py-6'>5+</h2>
+        <p className='text-center'>Years of experience</p>
+        <p className='text-center'>and counting</p>
+        </div>
+
+        <div className='w-full border rounded-md hover:scale-105 duration-300'>
+        <h2 className='text-2xl font-bold text-center py-6'>10+</h2>
+        <p className='text-center'>Countries served</p>
+        <p className='text-center'></p>
+        </div>
+
       </div>
     </div>
   )
